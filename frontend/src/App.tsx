6e05f70da26e943f6e6b7d5362ec5ebb3887830a
WindySilver <59417082+WindySilver@users.noreply.github.com>
2026-01-22 15:01:49 +0200

    JLPT checkboxes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,14 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [kanjiList, setkanjis] = useState<Kanji[]>([]);
+  const [jlptSelection, setJLPT] = useState<{ [key: string]: boolean }>({
+        one: false,
+        two: false,
+        three: false,
+        four: false,
+        five: false
+    });
+
 
   interface Kanji {
         kanji: string,
@@ -11,6 +19,14 @@ function App() {
         pronunciation_on_yomi: string,
         jlpt: number
 };
+
+  const handleJLPTSelection = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, checked } = e.target;
+        setJLPT((prevState) => ({
+            ...prevState,
+            [name]: checked
+        }));
+  }
 
   const fetchkanjis = async () => {
     const response = await axios.get("http://localhost:8080");
@@ -22,10 +38,50 @@ function App() {
   }, []);
 
   return (
-    <div >
+    <div>
       <h1>
         Kanji Generator
       </h1>
+
+      <div>
+        <p>Selected JLPT levels: </p>
+        <input
+                type="checkbox"
+                name="five"
+                checked={jlptSelection.five}
+                onChange={handleJLPTSelection}
+            />
+          <label>5</label>
+        <input
+                type="checkbox"
+                name="four"
+                checked={jlptSelection.four}
+                onChange={handleJLPTSelection}
+            />
+          <label>4</label>
+        <input
+                type="checkbox"
+                name="three"
+                checked={jlptSelection.three}
+                onChange={handleJLPTSelection}
+            />
+          <label>3</label>
+        <input
+                type="checkbox"
+                name="two"
+                checked={jlptSelection.two}
+                onChange={handleJLPTSelection}
+            />
+          <label>2</label>
+        <input
+                type="checkbox"
+                name="one"
+                checked={jlptSelection.one}
+                onChange={handleJLPTSelection}
+            />
+          <label>1</label>
+      </div>
+
         {kanjiList.map((kanji) => (
           <div key={kanji.kanji}>
             <h1>{kanji.kanji}</h1>
